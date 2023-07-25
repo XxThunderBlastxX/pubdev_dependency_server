36e21ae9e887009866edb9344682b4209941c7d8
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-07-25 13:22:31 +0530

    Modified version

--- controller/initial_controller.go
+++ controller/initial_controller.go
@@ -4,8 +4,7 @@ import "github.com/gofiber/fiber/v2"
 
 func InitialController() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		jsonData := fiber.Map{"Name": "Pub.dev Package Server", "Created By": "Koustav Mondal <ThunderBlast>", "Status": "Running", "Version": "0.0.1"}
-
-		return ctx.Status(200).JSON(jsonData)
+		jsonData := fiber.Map{"Name": "Pub.dev Package Server", "Created By": "Koustav Mondal <ThunderBlast>", "Status": "Running", "Version": "1.0.0"}
+		return ctx.Status(fiber.StatusOK).JSON(jsonData)
 	}
 }
